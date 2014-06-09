c7532504f6729dcd0cc2568129588959be1dc578
Mikito Takada <mikito.takada@gmail.com>
2014-06-08 18:19:22 -0700

    Performance improvements

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 var microee = require('microee');
 
+// setTimeout is very problematic since it consumes some resources on each invocation
+// and cannot be optimized like process.nextTick
+var delay = (process && process.nextTick ? process.nextTick : (setImmediate ? setImmediate : setTimeout));
+
 function Parallel(limit) {
   this.limit = limit || Infinity;
   this.running = 0;
@@ -20,10 +24,18 @@ Parallel.prototype.exec = function(tasks, onDone) {
 
   if(!tasks || tasks.length == 0) {
     onDone && onDone();
-    return this._exec();
+    this._next();
+    return this;
   }
 
-  this.tasks = this.tasks.concat(tasks);
+  if (Array.isArray(tasks)) {
+    // avoid concat for memory reasons
+    tasks.forEach(function(task) {
+      self.tasks.push(task);
+    });
+  } else {
+    this.tasks.push(tasks);
+  }
 
   function errHandler(err, task) {
     if(tasks.indexOf(task) > -1) {
@@ -51,52 +63,48 @@ Parallel.prototype.exec = function(tasks, onDone) {
     this.on('error', errHandler)
         .when('done', doneHandler);
   }
-  return this._exec();
+  this._next();
+  return this;
 };
 
-Parallel.prototype._exec = function() {
-  var self = this,
-      hadError = false;
-
-  function next() {
-    // if nothing is running and the queue is empty, emit empty
-    if(self.running == 0 && self.tasks.length == 0) {
-      self.emit('empty');
-    }
-    // if nothing is running, then we can safely clean the removed queue
-    if(self.running == 0) {
-      self.removed = [];
-    }
-    while(self.running < self.limit && self.tasks.length > 0) {
-      // need this IIFE so `task` can be referred to later on with the right value
-      self.running++;
-      (function(task) {
-        // avoid issues with deep recursion
-        setTimeout(function() {
-          // check that the task is still in the queue
-          // (as it may have been removed due to a failure)
-          if(self.removed.indexOf(task) > -1) {
-            self.running--;
-            next();
-            return;
-          }
-
-          task(function(err) {
-            self.running--;
-            if(err) {
-              return self.emit('error', err, task);
-            }
-            self.emit('done', task);
-            next();
-          });
-        }, 0);
-      })(self.tasks.shift());
-    }
+Parallel.prototype._next = function() {
+  var self = this;
+  // if nothing is running and the queue is empty, emit empty
+  if(self.running == 0 && self.tasks.length == 0) {
+    self.emit('empty');
   }
-  setTimeout(function() {
-    next();
+  // if nothing is running, then we can safely clean the removed queue
+  if(self.running == 0) {
+    self.removed = [];
+  }
+  while(self.running < self.limit && self.tasks.length > 0) {
+    // need this IIFE so `task` can be referred to later on with the right value
+    self.running++;
+    self._runTask(self.tasks.shift());
+  }
+};
+
+Parallel.prototype._runTask = function(task) {
+  var self = this;
+  // avoid issues with deep recursion
+  delay(function() {
+    // check that the task is still in the queue
+    // (as it may have been removed due to a failure)
+    if(self.removed.indexOf(task) > -1) {
+      self.running--;
+      self._next();
+      return;
+    }
+
+    task(function(err) {
+      self.running--;
+      if(err) {
+        return self.emit('error', err, task);
+      }
+      self.emit('done', task);
+      self._next();
+    });
   }, 0);
-  return this;
 };
 
 Parallel.prototype.removeTasks = function(tasks) {
